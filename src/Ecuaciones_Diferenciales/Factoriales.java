0b028d42a4fadeb1c0794454cbaa1901fe0c12bd
Manis <joseaustin.z9439@gmail.com>
2022-03-21 15:06:18 -0600

    Burbuja

---
+++ src/Ecuaciones_Diferenciales/Factoriales.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ecuaciones_Diferenciales;
+
+/**
+ *
+ * @author Manis
+ */
+public class Factoriales {
+    public void ventana(){
+        
+    }
+    void factorial()
+    {
+        //ax^2+bx+c
+        int a=0;//introducir a
+        int b=0;//Introduce x
+        int c=0;//introduce c
+        
+    }
+}
